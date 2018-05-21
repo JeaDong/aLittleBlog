d2b5e2b90cf3777d105930e107ba507c9011898d
JeaDong <704404536@qq.com>
2018-05-21 12:19:55 +0800

    fix page of post bug

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 # from routes.todo import main as todo_routes
 # from routes.comment import main as comment_routes
 # from routes.session_exp import main as session_routes
-# from routes.blog import main as blog_routes
+from routes.blog import main as blog_routes
 from routes.index import main as index_routes
 
 # web framework
@@ -25,7 +25,7 @@ app.secret_key = 'test for good'
 # app.register_blueprint(todo_routes, url_prefix='/todo')
 # app.register_blueprint(comment_routes, url_prefix='/comment')
 # app.register_blueprint(session_routes, url_prefix='/session')
-# app.register_blueprint(blog_routes, url_prefix='/blog')
+app.register_blueprint(blog_routes, url_prefix='/blog')
 app.register_blueprint(index_routes)
 
 
